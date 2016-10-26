cabe5fd350775186d4625d534537d1cf41584d0f
Konsta <konsta09@gmail.com>
2016-10-26 17:26:20 +0300

    build thermal daemon from source
    Change-Id: I3c43c7ba1ab036fcf01f69361c05c59c85a41a4b

--- cherrytrail.mk
+++ cherrytrail.mk
@@ -73,6 +73,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     pstore-clean
 
+# Thermal daemon
+PRODUCT_PACKAGES += \
+    thermal-daemon
+
+PRODUCT_COPY_FILES += \
+    external/thermal_daemon/data/thermal-conf.xml:system/etc/thermal-daemon/thermal-conf.xml \
+    external/thermal_daemon/data/thermal-cpu-cdev-order.xml:system/etc/thermal-daemon/thermal-cpu-cdev-order.xml
+
 # Wifi
 PRODUCT_PACKAGES += \
     hostapd \
