8c3c85b97cfdb77749060dea2adaebd23a33e252
Konsta <konsta09@gmail.com>
2016-10-29 13:01:59 +0300

    ramdisk: remove thermal-daemon
    Change-Id: I299d6788231c5bb3ce70da5a1e8723c9214e37c8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -30,7 +30,7 @@ TARGET_CPU_ABI_LIST := x86,armeabi-v7a,armeabi
 TARGET_CPU_ABI_LIST_32_BIT := x86,armeabi-v7a,armeabi
 
 # Kernel
-BOARD_KERNEL_CMDLINE := loglevel=5 androidboot.hardware=cherrytrail firmware_class.path=/system/etc/firmware i915.fastboot=1 vga=current i915.modeset=1 drm.vblankoffdelay=1 console=ttyS0,115200n8 bootboost=1 pm_suspend_debug=1 pstore.backend=ramoops
+BOARD_KERNEL_CMDLINE := loglevel=5 androidboot.hardware=cherrytrail firmware_class.path=/system/etc/firmware i915.fastboot=1 vga=current i915.modeset=1 drm.vblankoffdelay=1 console=ttyS0,115200n8 bootboost=1 pm_suspend_debug=1 pstore.backend=ramoops thermal.off=1
 BOARD_KERNEL_CMDLINE += androidboot.selinux=disabled
 
 # Binder
--- cherrytrail.mk
+++ cherrytrail.mk
@@ -71,14 +71,6 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
 PRODUCT_PACKAGES += \
     lights.gmin
 
-# Thermal daemon
-PRODUCT_PACKAGES += \
-    thermal-daemon
-
-PRODUCT_COPY_FILES += \
-    external/thermal_daemon/data/thermal-conf.xml:system/etc/thermal-daemon/thermal-conf.xml \
-    external/thermal_daemon/data/thermal-cpu-cdev-order.xml:system/etc/thermal-daemon/thermal-cpu-cdev-order.xml
-
 # Wifi
 PRODUCT_PACKAGES += \
     hostapd \
