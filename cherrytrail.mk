74e68d6629a65483f516acb7112fe7e8a89b902a
Konsta <konsta09@gmail.com>
2016-11-06 16:08:11 +0200

    increase dalvik heap size
    Change-Id: Ib1338aff0441aa027c84c3fd40990f56253d426a

--- cherrytrail.mk
+++ cherrytrail.mk
@@ -42,8 +42,8 @@ PRODUCT_PACKAGES += \
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapgrowthlimit=100m \
-    dalvik.vm.heapsize=174m \
+    dalvik.vm.heapgrowthlimit=144m \
+    dalvik.vm.heapsize=384m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=8m
