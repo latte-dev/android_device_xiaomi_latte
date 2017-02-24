83f289a7900230cdfddc3db77d1fb2e30869ace7
tank0412 <romabukhtiarov@gmail.com>
2017-02-24 22:13:25 +0300

    latte: use prebuilt kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,10 +180,10 @@ TARGET_OTA_ASSERT_DEVICE := latte
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 27783069696
 
 # Kernel
-BOARD_KERNEL_IMAGE_NAME := bzImage
-KERNEL_TOOLCHAIN := /opt/poky/1.8/sysroots/x86_64-pokysdk-linux/usr/bin/x86_64-poky-linux
-TARGET_KERNEL_ARCH := x86_64
-TARGET_KERNEL_CONFIG := xiaomi_latte_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := x86_64-poky-linux-
-TARGET_KERNEL_SOURCE := kernel/xiaomi/latte
+#BOARD_KERNEL_IMAGE_NAME := bzImage
+#KERNEL_TOOLCHAIN := /opt/poky/1.8/sysroots/x86_64-pokysdk-linux/usr/bin/x86_64-poky-linux
+#TARGET_KERNEL_ARCH := x86_64
+#TARGET_KERNEL_CONFIG := xiaomi_latte_defconfig
+#TARGET_KERNEL_CROSS_COMPILE_PREFIX := x86_64-poky-linux-
+#TARGET_KERNEL_SOURCE := kernel/xiaomi/latte
 
