b2f159122b3a9542ceb46761b9c5ada154bd5320
tank0412 <romabukhtiarov@gmail.com>
2017-02-25 09:07:04 +0300

    latte: switch to permissive selinux

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,7 @@ TARGET_CPU_ABI_LIST := x86,armeabi-v7a,armeabi
 TARGET_CPU_ABI_LIST_32_BIT := x86,armeabi-v7a,armeabi
 
 # Kernel
-BOARD_KERNEL_CMDLINE := loglevel=7 androidboot.hardware=latte firmware_class.path=/system/etc/firmware i915.fastboot=1 vga=current i915.modeset=1 drm.vblankoffdelay=1 bootboost=1 pm_suspend_debug=1 pstore.backend=ramoops
+BOARD_KERNEL_CMDLINE := loglevel=7 androidboot.hardware=latte firmware_class.path=/system/etc/firmware i915.fastboot=1 vga=current i915.modeset=1 drm.vblankoffdelay=1 bootboost=1 pm_suspend_debug=1 pstore.backend=ramoops androidboot.selinux=permissive
 
 # Binder
 TARGET_USES_64_BIT_BINDER := true
