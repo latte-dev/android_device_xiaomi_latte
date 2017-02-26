d47b152b89b0c6387ff24c0b9ce1593a66c08f58
Roman <romabukhtiarov@gmail.com>
2017-02-26 12:57:09 +0300

    latte: define legacy omx decoder support
    See this:
    https://github.com/LineageOS/android_device_asus_mofd-common/blob/cm-14.1/BoardConfigCommon.mk#L59
    
    ADDITIONAL_DEFAULT_PROPERTIES will add it to default.prop.
    NOTE: It is NOT a build.prop!
    default.prop located in the ramdisk!

--- latte.mk
+++ latte.mk
@@ -145,3 +145,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
+    
+
+# Legacy omx decoder suppor
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true
+
